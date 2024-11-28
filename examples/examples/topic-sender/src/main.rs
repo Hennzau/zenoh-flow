29ca3efd9bd109c09653bfcef816fa3cd410d92c
Enzo Le Van <dev@enzo-le-van.fr>
2024-11-28 18:17:15 +0100

    Update examples
    Update examples

---
+++ examples/examples/topic-sender/src/main.rs
@@ -0,0 +1,56 @@
+use std::path::PathBuf;
+
+use anyhow::anyhow;
+use clap::Parser;
+
+use zenoh::{
+    prelude::{r#async::AsyncResolve, *},
+    Result,
+};
+
+#[derive(Parser)]
+struct TopicSender {
+    /// The path to a Zenoh configuration to manage the connection to the Zenoh
+    /// network.
+    ///
+    /// If no configuration is provided, `zfctl` will default to connecting as
+    /// a peer with multicast scouting enabled.
+    #[arg(short = 'z', long, verbatim_doc_comment)]
+    zenoh_configuration: Option<PathBuf>,
+
+    /// The key_expression to use for the `put` operation.
+    #[arg(verbatim_doc_comment)]
+    key_expression: KeyExpr<'static>,
+
+    /// The payload to use for the `put` operation.
+    #[arg(verbatim_doc_comment)]
+    payload: String,
+}
+
+#[async_std::main]
+async fn main() -> Result<()> {
+    let args = TopicSender::parse();
+
+    let zenoh_config = match args.zenoh_configuration {
+        Some(path) => zenoh::prelude::Config::from_file(path.clone()).map_err(|e| {
+            anyhow!(
+                "Failed to parse the Zenoh configuration from < {} >:\n{e:?}",
+                path.display()
+            )
+        })?,
+        None => zenoh::config::peer(),
+    };
+
+    let session = zenoh::open(zenoh_config)
+        .res()
+        .await
+        .map_err(|e| anyhow!("Failed to open Zenoh session:\n{:?}", e))?;
+
+    session
+        .put(&args.key_expression, args.payload)
+        .res()
+        .await
+        .map_err(|e| anyhow!("Failed to put a sample:\n{:?}", e))?;
+
+    Ok(())
+}
